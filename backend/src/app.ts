0fd0cb4ba06e466c88978f5e74c7f6de737cda27
Madhusmitabehera16 <madhuusmitaa7848@gmail.com>
2026-06-13 21:51:12 +0530

    url change

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://jobblyfrontend.vercel.app/"
+      "https://jobbly-black.vercel.app/"
     ],
     credentials: true,
   })
